f068ea3aadac075b7199ab0cd6c7b71e32bd18d8
Gonéri Le Bouder <goneri@lebouder.net>
2021-05-12 10:26:45 -0400

    prepare a 0.0.1 release

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name="sphinx-ansible",
     description="Imagine a world where your documentation is also your Ansible playbook",
     version="0.0.1",
+    long_description=README,
+    long_description_content_type="text/markdown",
     author="Gonéri Le Bouder",
     author_email="goneri@lebouder.net",
     install_requires=["ansible-runner"],
-    url="https://github.com/goneri/sphinx-ansible",
+    url="https://github.com/ansible-community/sphinx-ansible",
     packages=["sphinxcontrib.sphinx_ansible"],
 )
