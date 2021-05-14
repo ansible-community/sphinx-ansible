ad2fab298e6806aaacbd402e1df430b3c5086f2c
Gonéri Le Bouder <goneri@lebouder.net>
2021-05-14 11:23:18 -0400

    prepare 0.0.2 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 setup(
     name="sphinx-ansible",
     description="Imagine a world where your documentation is also your Ansible playbook",
-    version="0.0.1",
+    version="0.0.2",
     long_description=README,
     long_description_content_type="text/markdown",
     author="Gonéri Le Bouder",
